b94e663c73018d1a5aeb10a0ebf30077ee63869e
Sudhamsu Krishna <smanne@cs.umass.edu>
2017-05-09 20:25:32 -0400

    Calculate both with and without box size doubling

--- calc_sectional_L2_distances.m
+++ calc_sectional_L2_distances.m
@@ -1,12 +1,10 @@
-% This script will generate clips with two initial bounding boxes on
-% patches. One patch will eventually occlude the other.
 
 debug = false;
 
 if_use_minus_something = false; % to use minus something frame instead of first to compare
 compare_past_by = 20; % matter only if if_use_minus_something is true
 
-pad = 100;
+pad = 100; % zero padding to manage doubled box going out of bounds
 
 seqStart = 1;
 seqEnd = 46;
@@ -67,7 +65,7 @@ for sNum = seqStart:seqEnd
 
                 trackboxes = dlmread(fullfile(boxdir,sprintf('%s_%03d.track',chunk_name,bbox_i)));
 
-                im_dists = zeros(size(trackboxes));
+                im_dists = zeros(size(trackboxes,1),2*size(trackboxes,2));
                 
                 has_not_run_before = true;
                 
@@ -86,21 +84,25 @@ for sNum = seqStart:seqEnd
                         frame_first = imread(fullfile(imDir,imFiles{minFrame+prior_frame_index-1}));
                         frame_first = padarray(frame_first, [pad,pad], 0, 'both');
 
-                        x_first = bbox_first(1);
-                        y_first = bbox_first(2);
+                        x_first = bbox_first(1) + pad;
+                        y_first = bbox_first(2) + pad;
                         w_first = bbox_first(3);
                         h_first = bbox_first(4);
 
-                        x_first = x_first-floor(w_first/2) + pad;
-                        y_first = y_first-floor(h_first/2) + pad;
-                        w_first = 2*w_first;
-                        h_first = 2*h_first;
-                        % TODO Check if it crosses image boundary?
+                        x_first_doubled = x_first-floor(w_first/2);
+                        y_first_doubled = y_first-floor(h_first/2);
+                        w_first_doubled = 2*w_first;
+                        h_first_doubled = 2*h_first;
 
                         bbox_left_first = [x_first,y_first,floor(w_first/2),h_first];
                         bbox_right_first = [x_first+floor(w_first/2),y_first,floor(w_first/2),h_first];
                         bbox_up_first = [x_first,y_first,w_first,floor(h_first/2)];
                         bbox_down_first = [x_first,y_first+floor(h_first/2),w_first,floor(h_first/2)];
+                        
+                        bbox_left_first_doubled = [x_first_doubled,y_first_doubled,floor(w_first_doubled/2),h_first_doubled];
+                        bbox_right_first_doubled = [x_first_doubled+floor(w_first_doubled/2),y_first_doubled,floor(w_first_doubled/2),h_first_doubled];
+                        bbox_up_first_doubled = [x_first_doubled,y_first_doubled,w_first_doubled,floor(h_first_doubled/2)];
+                        bbox_down_first_doubled = [x_first_doubled,y_first_doubled+floor(h_first_doubled/2),w_first_doubled,floor(h_first_doubled/2)];
                         
                         has_not_run_before = false;
                     end
@@ -111,29 +113,38 @@ for sNum = seqStart:seqEnd
                     frame = imread(fullfile(imDir,imFiles{frame_i-minFrame+1}));
                     frame = padarray(frame, [pad,pad], 0, 'both');
                     
-                    x = bbox(1);
-                    y = bbox(2);
+                    x = bbox(1) + pad;
+                    y = bbox(2) + pad;
                     w = bbox(3);
                     h = bbox(4);
 
                     % INCREASE BOX SIZE
-                    x = x-floor(w/2) + pad;
-                    y = y-floor(h/2) + pad;
-                    w = 2*w;
-                    h = 2*h;
-                    % TODO Check if it crosses image boundary?
+                    x_doubled = x-floor(w/2);
+                    y_doubled = y-floor(h/2);
+                    w_doubled = 2*w;
+                    h_doubled = 2*h;
 
                     bbox_left = [x,y,floor(w/2),h];
                     bbox_right = [x+floor(w/2),y,floor(w/2),h];
                     bbox_up = [x,y,w,floor(h/2)];
                     bbox_down = [x,y+floor(h/2),w,floor(h/2)];
+                    
+                    bbox_left_doubled = [x_doubled,y_doubled,floor(w_doubled/2),h_doubled];
+                    bbox_right_doubled = [x_doubled+floor(w_doubled/2),y_doubled,floor(w_doubled/2),h_doubled];
+                    bbox_up_doubled = [x_doubled,y_doubled,w_doubled,floor(h_doubled/2)];
+                    bbox_down_doubled = [x_doubled,y_doubled+floor(h_doubled/2),w_doubled,floor(h_doubled/2)];
 
                     dist_left_l2 = calc_bbox_distance(bbox_left,bbox_left_first,frame,frame_first);
                     dist_right_l2 = calc_bbox_distance(bbox_right,bbox_right_first,frame,frame_first);
                     dist_up_l2 = calc_bbox_distance(bbox_up,bbox_up_first,frame,frame_first);
                     dist_down_l2 = calc_bbox_distance(bbox_down,bbox_down_first,frame,frame_first);
                     
-                    im_dists(frame_i-minFrame+1, :) = [dist_left_l2, dist_right_l2, dist_up_l2, dist_down_l2];
+                    dist_left_l2_doubled = calc_bbox_distance(bbox_left_doubled,bbox_left_first_doubled,frame,frame_first);
+                    dist_right_l2_doubled = calc_bbox_distance(bbox_right_doubled,bbox_right_first_doubled,frame,frame_first);
+                    dist_up_l2_doubled = calc_bbox_distance(bbox_up_doubled,bbox_up_first_doubled,frame,frame_first);
+                    dist_down_l2_doubled = calc_bbox_distance(bbox_down_doubled,bbox_down_first_doubled,frame,frame_first);
+                    
+                    im_dists(frame_i-minFrame+1, :) = [dist_left_l2, dist_right_l2, dist_up_l2, dist_down_l2, dist_left_l2_doubled, dist_right_l2_doubled, dist_up_l2_doubled, dist_down_l2_doubled];
                 end
                 
                 dlmwrite(fullfile(boxdir,sprintf('%s_%03d.4boxdist',chunk_name,bbox_i)),im_dists);
@@ -142,13 +153,18 @@ for sNum = seqStart:seqEnd
     end
 end
 
+fprintf('\n\n');
 
 %% plot
 if debug
     x = minFrame:1:maxFrame;
 
-    plot(x,im_dists(:,1),'Color','r'); hold on;
-    plot(x,im_dists(:,2),'Color','g'); hold on;
-    plot(x,im_dists(:,3),'Color','b'); hold on;
-    plot(x,im_dists(:,4),'Color','k');
+    plot(x,im_dists(:,1),'r'); hold on;
+    plot(x,im_dists(:,2),'g'); hold on;
+    plot(x,im_dists(:,3),'b'); hold on;
+    plot(x,im_dists(:,4),'k'); hold on;
+    plot(x,im_dists(:,5),'r--'); hold on;
+    plot(x,im_dists(:,6),'g--'); hold on;
+    plot(x,im_dists(:,7),'b--'); hold on;
+    plot(x,im_dists(:,8),'k--');
 end
